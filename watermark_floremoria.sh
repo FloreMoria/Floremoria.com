d830ed22cb116b4850a81fc71d0b3609e91198c9
FloreMoria <staff.floremoria@gmail.com>
2026-03-14 12:10:20 +0100

    update immagini webp e script watermark

--- watermark_floremoria.sh
+++ watermark_floremoria.sh
@@ -2,22 +2,19 @@
 
 WATERMARK="/Users/floremoria/Downloads/Floremoria_dot_com/floremoria/public/images/brand/Logo FloreMoria WaterMark 2026.png"
 
-# Cartella output
 mkdir -p watermarked
-
-# Bash: ignora pattern vuoti
-shopt -s nullglob
 
 for img in *.jpg *.jpeg *.png; do
   [ -f "$img" ] || continue
 
   magick "$img" \
     \( "$WATERMARK" \
-       -colorspace sRGB \
-       -fuzz 18% \
-       -transparent black \
        -alpha on \
-       -channel A -evaluate multiply 0.22 +channel \
+       -fuzz 15% \
+       -transparent black \
+       -fill "#B8B8B8" \
+       -colorize 100 \
+       -channel A -evaluate multiply 0.28 +channel \
        -resize 18% \
     \) \
     -gravity southeast \
